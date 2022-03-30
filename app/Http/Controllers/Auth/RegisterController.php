d1b6db08bbe52931646ce669024dbab0f8f4162f
Jeff McHale <rhithik@pop-os.localdomain>
2022-03-29 23:22:19 -0400

    first commit

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    public function index()
+    {
+        return view('auth.register');
+    }
+
+    public function store(Request $request)
+    {
+        //validation
+        $this->validate($request, [
+            'name' => 'required|max:255',
+            'username' => 'required|max:255',
+            'email' => 'required|email|max:255',
+            'password' => 'required|confirmed',
+        ]);
+        //store user
+        //sign the user in
+        //redirect
+    }
+}
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <title>Posty</title>
+</head>
+<body class="bg-gray-200">
+<nav class="p-6 bg-white flex justify-between mb-6">
+    <ul class="flex items-center">
+        <li class="px-3"><a href="">Home</a></li>
+        <li class="px-3"><a href="">Dashboard</a></li>
+        <li class="px-3"><a href="">Post</a></li>
+    </ul>
+    <ul class="flex items-center">
+        <li class="px-3"><a href="">Jeff McHale</a></li>
+        <li class="px-3"><a href="">Login</a></li>
+        <li class="px-3"><a href="{{ route('register') }}">Register</a></li>
+        <li class="px-3"><a href="">Logout</a></li>
+    </ul>
+</nav>
+    @yield('content')
+</body>
+</html>
